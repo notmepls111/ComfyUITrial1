73c924bda6a7bc9dc34f53ede56527025fc0554f
notmepls111 <140951966+notmepls111@users.noreply.github.com>
2023-07-30 16:54:40 +0530

    Update run_nvidia_gpu.bat

--- .ci/nightly/windows_base_files/run_nvidia_gpu.bat
+++ .ci/nightly/windows_base_files/run_nvidia_gpu.bat
@@ -1,2 +1,2 @@
-.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --use-pytorch-cross-attention
+.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --use-pytorch-cross-attention --normalvram --fp16-vae
 pause
